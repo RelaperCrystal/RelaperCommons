cf74f371052f72eedb61c466d1852d1a420f3d04
RelaperCrystal <RelaperCrystal@163.com>
2021-04-18 18:20:05 +0800

    :sparkles: Add TryGetDisplayNameOfVehicleModel method

--- RelaperCommons/VehicleExtensions.cs
+++ RelaperCommons/VehicleExtensions.cs
@@ -12,16 +12,42 @@ namespace RelaperCommons
         /// Gets the localized display name of the vehicle.
         /// </summary>
         /// <param name="vehicle">The vehicle.</param>
-        /// <returns>The display name of vehicle, or <see cref="string.Empty"/> if not found.</returns>
+        /// <returns>The display name of vehicle, or <see langword="null"/> if not found.</returns>
         public static string GetDisplayName(this Vehicle vehicle)
         {
             var name = NativeFunction.Natives.GET_DISPLAY_NAME_FROM_VEHICLE_MODEL<string>(vehicle.Model.Hash);
             if (string.IsNullOrWhiteSpace(name) || name == "CARNOTFOUND")
             {
-                return string.Empty;
+                return null;
             }
 
             return Game.GetLocalizedString(name);
+        }
+
+        /// <summary>Tries to get the display name of specified vehicle model.</summary>
+        /// <param name="vehicle">The vehicle.</param>
+        /// <param name="result">The result.</param>
+        /// <returns>
+        ///   <para>Returns true if a proper name has been found; otherwise, false.</para>
+        /// </returns>
+        public static bool TryGetDisplayNameOfVehicleModel(this Vehicle vehicle, out string result)
+        {
+            var name = NativeFunction.Natives.GET_DISPLAY_NAME_FROM_VEHICLE_MODEL<string>(vehicle.Model.Hash);
+            if (name == "CARNOTFOUND")
+            {
+                result = string.Empty;
+                return false;
+            }
+
+            var tempResult = Game.GetLocalizedString(name);
+            if (string.IsNullOrWhiteSpace(tempResult))
+            {
+                result = string.Empty;
+                return false;
+            }
+
+            result = tempResult;
+            return true;
         }
     }
 }
